97c2e98c8e1bad4bcf28a9ecb7369f844e887e08
Dmitry Zhdankin <dmitryzhdankin@Dmitrys-MacBook-Pro.local>
2018-05-28 15:43:15 -0700

    added maps project

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+# @app.route('/distance')
+# def distance():
+#     return render_template('distance.html')
+
+@app.route('/distance-kendall')
+def distance_kendall():
+    return render_template('distance-kendall.html')
